277b9d82e7591d484eb702f001a2e559c783e6d7
Akshay-ingle <akshy.nilesh.ingle@gmail.com>
2024-11-19 18:56:34 +0530

    chalanges

---
+++ pythonchlanges.py
@@ -0,0 +1,15 @@
+a=3
+b=4
+c=65
+d=8
+e=9
+f=(a+b)-c/d*a;
+
+print('the answer is given here:',f)
+g="akshay"
+age=0
+if g=="akshay" or g=="aarv" and age>=2:
+   print('wellcome')
+
+else:
+   print('thanks to atempt')   
